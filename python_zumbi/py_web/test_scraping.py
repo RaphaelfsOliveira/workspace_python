a0c1050f3ec9f6f5d80678a168e6a404df687660
roliveira <raphaelbrf@gmail.com>
2017-02-02 15:08:08 -0200

    change url test

--- python_zumbi/py_web/test_scraping.py
+++ python_zumbi/py_web/test_scraping.py
@@ -1,7 +1,6 @@
-import urllib.request
+import urllib.request #modulo que permite conversar com a internet
 pagina = urllib.request.urlopen(
-    'http://www.prezunic.com.br/Ofertas.aspx?tipo=Oferta%20do%20dia&&Area=Ofertas'
-    '''http://beans.itcarlow.ie/prices.html''')
+    'http://beans.itcarlow.ie/prices-loyalty.html')
 text = pagina.read().decode('utf8')
 print(text)
 preco = text[234:238]
