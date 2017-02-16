436f384452fa9b9565daf7dc61bcd61c2c948790
roliveira <raphaelbrf@gmail.com>
2017-02-16 18:39:39 -0200

    up

--- mark_crawler/mark_II_crawler.py
+++ mark_crawler/mark_II_crawler.py
@@ -1,14 +1,15 @@
-from urllib import request, parse
+from urllib import request, parse, error
 import string
 
 url = 'http://www.epocacosmeticos.com.br/'
-link_i = 'departamentos'
-link_f = 'ofertas'
 
-
-def search_urls(url, link_i, link_f):
+def search_urls(url):
 	
-	pag = request.urlopen(url)
+	try:
+		pag = request.urlopen(url)
+	except HTTPError as e:
+		content = e.read()
+
 	text = pag.read().decode('utf8')
 
 	#string.punctuation
@@ -16,40 +17,33 @@ def search_urls(url, link_i, link_f):
 		text = text.replace(c, ' ')
 	text = text.split()
 	#print(text)
-
-	l_type = {}
-	cap = False
+	
+	l_type = list()
 
 	for word in text:
 
-		if 'href' in word.lower():
+		if word.lower().startswith('href'):
 			l_word = word.replace('href=', '').replace('"', '')
-			p_word = parse.urlparse(l_word)
-			l_type[p_word.path] = p_word.geturl()
-			#print('URL: ', p_word.geturl())
+			#p_word = parse.urlparse(l_word)
+			l_type.append(l_word)
 			
-			
-
 	return l_type
+
 
 if __name__ == '__main__':
 
 	print()
 
-	dictt = search_urls(url, link_i, link_f)
+	links = search_urls(url)
+	links2 = []
 
-	for key in dictt:
-		print('chave [ %s ] = %s' %(key, dictt[key]))
-		print()
-'''
-	for key in dictt.values():
+	#print(links)
 
-		url = key
-		dict2 = search_urls(url, 'categorias', 'botao-submenu')
+	for link in links:
+		if link.startswith('http'):
+			#print(link)
+			links2 = search_urls(link)
 
-	for key in dict2:
-		print('chave [ %s ] = %s' %(key, dict2[key]))
-		print()
-'''
-#print(dictt.values())
+		
 
+	
